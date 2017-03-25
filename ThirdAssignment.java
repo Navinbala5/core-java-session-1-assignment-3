1456ef013aa32babbd4627f832ca8ae6b7af478b
Navinbala5 <bnavin28@gmail.com>
2017-03-25 08:57:13 +0530

    Add files via upload

---
+++ ThirdAssignment.java
@@ -0,0 +1,17 @@
+
+public class ThirdAssignment {
+
+	public static void main(String[] args) {
+		int x=100;
+		int y=200;
+	System.out.println("before swapping the value of x is "+x);
+	System.out.println("before swapping the value of y is "+y);
+	x=x+y;
+	y=x-y;
+	x=x-y;
+	System.out.println("after swapping the value of x is "+x);
+	System.out.println("after swapping the value of y is "+y);
+
+	}
+
+}
